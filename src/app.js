2370b098dd9e54c5a73048fe59dacf41e8e8270f
Kate Deras <65469487+quackate@users.noreply.github.com>
2023-06-23 17:59:36 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  //My code begins right here!
+  const domainNameGenerator = () => {
+    let pronoun = ["the", "our"];
+    let adjective = ["great", "awesome", "crazy", "colorful", "brilliant"];
+    let subject = ["tiger", "apple", "pancake", "tortilla", "narwhal"];
+    let extension = [".com", ".es", ".en", ".org", ".net"];
+    let result = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adjective.length; j++) {
+        for (let k = 0; k < subject.length; k++) {
+          for (let l = 0; l < extension.length; l++) {
+            let domainName =
+              pronoun[i] + adjective[j] + subject[k] + extension[l];
+
+            result.push(domainName);
+          }
+        }
+      }
+    }
+
+    return result;
+  };
+
+  console.log(domainNameGenerator());
 };
